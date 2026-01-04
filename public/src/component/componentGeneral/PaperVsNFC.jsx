b1afb4c42238fb0fff5d7572a1c3ee9eb49ccded
Jakaria <jakariait@gmail.com>
2026-01-04 20:55:08 +0600

    Update

--- public/src/component/componentGeneral/PaperVsNFC.jsx
+++ public/src/component/componentGeneral/PaperVsNFC.jsx
@@ -12,334 +12,112 @@ import imgSiCheckCircleFill1 from "../../assets/PaperVsNFC/siCheckCircleFill1.sv
 
 const PaperVsNfc = () => {
   return (
-    <div
-      className="bg-[#4b4c79]  rounded-[12px] size-full"
-      data-node-id="1:4131"
-    >
-      <div
-        className="absolute h-[649px] left-0 rounded-[12px] top-0 w-[963px]"
-        data-node-id="1:4132"
-      />
-      <div
-        className="absolute content-stretch flex gap-[33px] items-center left-[17px] top-[26px]"
-        data-node-id="1:4133"
+    <div>
+      <h1
+        style={{
+          width: "100%",
+          textAlign: "center",
+          color: "#4E52FB",
+          fontSize: 36,
+          fontWeight: "400",
+          wordWrap: "break-word",
+        }}
+        className={"py-10"}
       >
-        <div
-          className="h-[548px] relative shrink-0 w-[448px]"
-          data-node-id="1:4134"
-        >
-          <div
-            className="absolute bg-[#2d2d5f] h-[548px] left-0 rounded-[12px] top-0 w-[448px]"
-            data-node-id="1:4135"
-          />
-          <p
-            className="absolute font-['Bruno_Ace:Regular',sans-serif] leading-[normal] left-[67px] not-italic text-[#d1d2f1] text-[24px] top-[11px]"
-            data-node-id="1:4136"
-          >
-            Paper Business Card
-          </p>
-          <div
-            className="absolute h-[221px] left-0 rounded-[8px] top-[47px] w-[448px]"
-            data-node-id="1:4137"
-          >
+        Paper Business Card VS NFC Card
+      </h1>
+
+      <div className="bg-[#4b4c79] rounded-[12px] p-4 flex flex-col items-center gap-8 max-w-3xl mx-auto">
+        <div className="flex md:flex-row flex-col gap-8">
+          <div className="bg-[#2d2d5f] rounded-[12px] flex flex-col gap-4 flex-1">
+            <p className=" text-[#d1d2f1] text-2xl py-2 text-center">
+              Paper Business Card
+            </p>
             <img
               alt=""
-              className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none rounded-[8px] size-full"
+              className="rounded-[8px] h-[200px] w-full"
               src={imgRectangle5276}
             />
-          </div>
-          <div
-            className="absolute content-stretch flex flex-col gap-[30px] items-start left-[50px] top-[280px] w-[185px]"
-            data-node-id="1:4138"
-          >
-            <div
-              className="content-stretch flex gap-[12px] items-center relative shrink-0 w-full"
-              data-node-id="1:4139"
-            >
-              <div
-                className="relative shrink-0 size-[36px]"
-                data-name="mdi:information"
-                data-node-id="1:4140"
-              >
-                <img
-                  alt=""
-                  className="block max-w-none size-full"
-                  src={imgMdiInformation}
-                />
-              </div>
-              <div
-                className="content-stretch flex flex-col font-['Bruno_Ace:Regular',sans-serif] items-start leading-[normal] not-italic relative shrink-0 text-white w-[137px] whitespace-pre-wrap"
-                data-node-id="1:4142"
-              >
-                <p
-                  className="relative shrink-0 text-[12px] w-full"
-                  data-node-id="1:4143"
-                >
-                  Information
-                </p>
-                <p
-                  className="relative shrink-0 text-[8px] w-full"
-                  data-node-id="1:4144"
-                >
-                  Limited (Name, Phone, Email)
-                </p>
-              </div>
-            </div>
-            <div
-              className="content-stretch flex gap-[12px] items-center relative shrink-0 w-full"
-              data-node-id="1:4145"
-            >
-              <div
-                className="relative shrink-0 size-[36px]"
-                data-name="material-symbols:ad-group-off"
-                data-node-id="1:4146"
-              >
-                <img
-                  alt=""
-                  className="block max-w-none size-full"
-                  src={imgMaterialSymbolsAdGroupOff}
-                />
-              </div>
-              <div
-                className="content-stretch flex flex-col font-['Bruno_Ace:Regular',sans-serif] items-start leading-[normal] not-italic relative shrink-0 text-white w-[137px] whitespace-pre-wrap"
-                data-node-id="1:4148"
-              >
-                <p
-                  className="relative shrink-0 text-[12px] w-full"
-                  data-node-id="1:4149"
-                >
-                  Reprint Need
-                </p>
-                <p
-                  className="relative shrink-0 text-[8px] w-full"
-                  data-node-id="1:4150"
-                >
-                  Hand to Hand
-                </p>
-              </div>
-            </div>
-            <div
-              className="content-stretch flex gap-[12px] items-center relative shrink-0 w-full"
-              data-node-id="1:4151"
-            >
-              <div
-                className="relative shrink-0 size-[36px]"
-                data-name="si:close-circle-fill"
-                data-node-id="1:4152"
-              >
-                <img
-                  alt=""
-                  className="block max-w-none size-full"
-                  src={imgSiCloseCircleFill}
-                />
-              </div>
-              <div
-                className="content-stretch flex flex-col font-['Bruno_Ace:Regular',sans-serif] items-start leading-[normal] not-italic relative shrink-0 text-white w-[137px] whitespace-pre-wrap"
-                data-node-id="1:4154"
-              >
-                <p
-                  className="relative shrink-0 text-[12px] w-full"
-                  data-node-id="1:4155"
-                >
-                  Paper Waste
-                </p>
-                <p
-                  className="relative shrink-0 text-[8px] w-full"
-                  data-node-id="1:4156"
-                >
-                  Easy to Dammage
-                </p>
-              </div>
-            </div>
-            <div
-              className="content-stretch flex gap-[12px] items-center relative shrink-0 w-full"
-              data-node-id="1:4157"
-            >
-              <div
-                className="relative shrink-0 size-[36px]"
-                data-name="si:check-circle-fill"
-                data-node-id="1:4158"
-              >
-                <img
-                  alt=""
-                  className="block max-w-none size-full"
-                  src={imgSiCheckCircleFill}
-                />
-              </div>
-              <div
-                className="content-stretch flex flex-col items-start relative shrink-0 w-[137px]"
-                data-node-id="1:4160"
-              >
-                <p
-                  className="font-['Bruno_Ace:Regular',sans-serif] leading-[normal] not-italic relative shrink-0 text-[12px] text-white w-full whitespace-pre-wrap"
-                  data-node-id="1:4161"
-                >
-                  Passive
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div
-          className="h-[548px] relative shrink-0 w-[448px]"
-          data-node-id="1:4162"
-        >
-          <div
-            className="absolute bg-[#050b23] h-[548px] left-0 rounded-[12px] top-0 w-[448px]"
-            data-node-id="1:4163"
-          />
-          <p
-            className="absolute font-['Bruno_Ace:Regular',sans-serif] leading-[normal] left-[152px] not-italic text-[#d1d2f1] text-[24px] top-[8px]"
-            data-node-id="1:4164"
-          >
-            NFC Card
-          </p>
-          <div
-            className="absolute h-[221px] left-0 rounded-[8px] top-[47px] w-[448px]"
-            data-node-id="1:4165"
-          >
-            <img
-              alt=""
-              className="absolute inset-0 max-w-none object-50%-50% object-cover pointer-events-none rounded-[8px] size-full"
-              src={imgRectangle5277}
-            />
-          </div>
-          <div
-            className="absolute content-stretch flex flex-col gap-[30px] items-start left-[67px] top-[289px] w-[234px]"
-            data-node-id="1:4166"
-          >
-            <div
-              className="content-stretch flex gap-[12px] items-center relative shrink-0"
-              data-node-id="1:4167"
-            >
-              <div
-                className="relative shrink-0 size-[36px]"
-                data-name="material-symbols:add-circle-rounded"
-                data-node-id="1:4168"
-              >
-                <img
-                  alt=""
-                  className="block max-w-none size-full"
-                  src={imgMaterialSymbolsAddCircleRounded}
-                />
-              </div>
-              <div
-                className="content-stretch flex flex-col font-['Bruno_Ace:Regular',sans-serif] items-start leading-[normal] not-italic relative shrink-0 text-[#d1d2f1] w-[137px] whitespace-pre-wrap"
-                data-node-id="1:4170"
-              >
-                <p
-                  className="relative shrink-0 text-[12px] w-full"
-                  data-node-id="1:4171"
-                >
-                  Information
-                </p>
-                <p
-                  className="relative shrink-0 text-[8px] w-full"
-                  data-node-id="1:4172"
-                >
-                  UnLimited
-                </p>
-              </div>
-            </div>
-            <div
-              className="content-stretch flex gap-[12px] items-center relative shrink-0 w-full"
-              data-node-id="1:4173"
-            >
-              <div
-                className="overflow-clip relative shrink-0 size-[36px]"
-                data-name="solar:smartphone-update-bold"
-                data-node-id="1:4174"
-              >
-                <div
-                  className="absolute inset-[4.17%_12.5%_0_8.33%]"
-                  data-name="Group"
-                  data-node-id="1:4175"
-                >
-                  <img
-                    alt=""
-                    className="block max-w-none size-full"
-                    src={imgGroup}
-                  />
+            <div className="flex flex-col gap-4 px-10 py-4">
+              <div className="flex items-center gap-4">
+                <img alt="" className="w-9 h-9" src={imgMdiInformation} />
+                <div className="text-white">
+                  <p className="text-sm">Information</p>
+                  <p className="text-xs">Limited (Name, Phone, Email)</p>
                 </div>
               </div>
-              <div
-                className="content-stretch flex flex-col font-['Bruno_Ace:Regular',sans-serif] items-start leading-[normal] not-italic relative shrink-0 text-[#d1d2f1] w-[186px] whitespace-pre-wrap"
-                data-node-id="1:4178"
-              >
-                <p
-                  className="relative shrink-0 text-[12px] w-[186px]"
-                  data-node-id="1:4179"
-                >
-                  Update anytime digitally
-                </p>
-                <p
-                  className="min-w-full relative shrink-0 text-[8px] w-[min-content]"
-                  data-node-id="1:4180"
-                >
-                  Tap or Scan
-                </p>
-              </div>
-            </div>
-            <div
-              className="content-stretch flex gap-[12px] items-center relative shrink-0"
-              data-node-id="1:4181"
-            >
-              <div
-                className="relative shrink-0 size-[36px]"
-                data-name="streamline:investment-selection-remix"
-                data-node-id="1:4182"
-              >
+              <div className="flex items-center gap-4">
                 <img
                   alt=""
-                  className="block max-w-none size-full"
+                  className="w-9 h-9"
+                  src={imgMaterialSymbolsAdGroupOff}
+                />
+                <div className="text-white">
+                  <p className="text-sm">Reprint Need</p>
+                  <p className="text-xs">Hand to Hand</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-4">
+                <img alt="" className="w-9 h-9" src={imgSiCloseCircleFill} />
+                <div className="text-white">
+                  <p className="text-sm">Paper Waste</p>
+                  <p className="text-xs">Easy to Dammage</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-4">
+                <img alt="" className="w-9 h-9" src={imgSiCheckCircleFill} />
+                <p className=" text-sm text-white">Passive</p>
+              </div>
+            </div>
+          </div>
+          <div className="bg-[#050b23] rounded-[12px]  flex flex-col gap-4 flex-1">
+            <p className=" text-[#d1d2f1] text-2xl text-center py-2">
+              NFC Card
+            </p>
+            <img
+              alt=""
+              className="rounded-[8px] h-[200px] w-full"
+              src={imgRectangle5277}
+            />
+            <div className="flex flex-col gap-4 px-10 py-4">
+              <div className="flex items-center gap-4">
+                <img
+                  alt=""
+                  className="w-9 h-9"
+                  src={imgMaterialSymbolsAddCircleRounded}
+                />
+                <div className=" text-[#d1d2f1]">
+                  <p className="text-sm">Information</p>
+                  <p className="text-xs">UnLimited</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-4">
+                <img alt="" className="w-9 h-9" src={imgGroup} />
+                <div className="text-[#d1d2f1]">
+                  <p className="text-sm">Update anytime digitally</p>
+                  <p className="text-xs">Tap or Scan</p>
+                </div>
+              </div>
+              <div className="flex items-center gap-4">
+                <img
+                  alt=""
+                  className="w-9 h-9"
                   src={imgStreamlineInvestmentSelectionRemix}
                 />
+                <p className=" text-sm text-[#d1d2f1]">One Time Investment</p>
               </div>
-              <div
-                className="content-stretch flex flex-col items-start relative shrink-0 w-[158px]"
-                data-node-id="1:4184"
-              >
-                <p
-                  className="font-['Bruno_Ace:Regular',sans-serif] leading-[normal] not-italic relative shrink-0 text-[#d1d2f1] text-[12px] w-[186px] whitespace-pre-wrap"
-                  data-node-id="1:4185"
-                >
-                  One Time Investment
-                </p>
+              <div className="flex items-center gap-4">
+                <img alt="" className="w-9 h-9" src={imgSiCheckCircleFill1} />
+                <p className="text-sm text-[#d1d2f1]">Resuable & paperless</p>
               </div>
-            </div>
-            <div
-              className="content-stretch flex gap-[12px] items-center relative shrink-0 w-full"
-              data-node-id="1:4186"
-            >
-              <div
-                className="relative shrink-0 size-[36px]"
-                data-name="si:check-circle-fill"
-                data-node-id="1:4187"
-              >
-                <img
-                  alt=""
-                  className="block max-w-none size-full"
-                  src={imgSiCheckCircleFill1}
-                />
-              </div>
-              <p
-                className="font-['Bruno_Ace:Regular',sans-serif] leading-[normal] not-italic relative shrink-0 text-[#d1d2f1] text-[12px] w-[186px] whitespace-pre-wrap"
-                data-node-id="1:4189"
-              >{`Resuable & paperless`}</p>
             </div>
           </div>
         </div>
-      </div>
-      <div
-        className="absolute bg-[#271a42] content-stretch flex items-center justify-center left-[53px] p-[10px] rounded-[4px] top-[586px]"
-        data-node-id="1:4190"
-      >
-        <p
-          className="font-['Bruno_Ace:Regular',sans-serif] leading-[normal] not-italic relative shrink-0 text-[#d1d2f1] text-[24px]"
-          data-node-id="1:4191"
-        >
-          Upgrade From paper to smart NFC business Card today.
-        </p>
+        <div className="bg-[#271a42] rounded-[4px] p-4">
+          <p className="text-[#d1d2f1] text-2xl text-center">
+            Upgrade From paper to smart NFC business Card today.
+          </p>
+        </div>
       </div>
     </div>
   );
